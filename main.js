970a8f0a853475694999d16a41081da34f3d75e8
Aung <zyazzya@126.com>
2017-04-13 12:28:12 +0800

    地图

---
+++ main.js
@@ -0,0 +1,52 @@
+var myChart = echarts.init(document.getElementById('main'));
+function showProvince() {
+     myChart.showLoading();
+
+    $.get('chongqing.json', function (geoJson) {
+
+        myChart.hideLoading();
+
+        echarts.registerMap(name, geoJson);
+
+        myChart.setOption(option = {
+            backgroundColor: '#404a59',
+            title: {
+                text: provincesText[currentIdx],
+                left: 'center',
+                textStyle: {
+                    color: '#fff'
+                }
+            },
+            series: [
+                {
+                    type: 'map',
+                    mapType: 'chongqing',
+                    label: {
+                        emphasis: {
+                            textStyle: {
+                                color: '#fff'
+                            }
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            borderColor: '#389BB7',
+                            areaColor: '#fff',
+                        },
+                        emphasis: {
+                            areaColor: '#389BB7',
+                            borderWidth: 0
+                        }
+                    },
+                    animation: false
+                    // animationDurationUpdate: 1000,
+                    // animationEasingUpdate: 'quinticInOut'
+                }
+            ]
+        });
+    });
+}
+
+var currentIdx = 0;
+
+showProvince();
